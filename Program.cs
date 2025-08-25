72d6223b5039e39a463c714813a20b2958a2dde8
Lucas Cruz <138832464+MurdokZerah@users.noreply.github.com>
2025-08-25 15:35:21 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== Test iPhone ===");
+        Smartphone iphone = new Iphone("97777-1111", "iPhone 15 Pro Max", "111222333444", 256);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Instagram");
+        iphone.ReceberLigacao();
+
+        Console.WriteLine();
+
+        Console.WriteLine("=== Test Nokia ===");
+        Smartphone nokia = new Nokia("98888-2222", "Nokia Tijolão", "555666777888", 32);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+        nokia.ReceberLigacao();
+    }
+}
